5a3d3ef86aa07ab63db6e0522a3769f2eb13990f
Nikhil <nikhilgarg72@gmail.com>
2017-05-18 15:20:39 +0530

    Multiple components

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -9,6 +9,7 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms"); // <-- NgModel lives here
 var app_component_1 = require("./app.component");
+var hero_detail_component_1 = require("./hero-detail.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -19,7 +20,10 @@ AppModule = __decorate([
         imports: [platform_browser_1.BrowserModule,
             forms_1.FormsModule // <-- import the FormsModule before binding with [(ngModel)] 
         ],
-        declarations: [app_component_1.AppComponent],
+        declarations: [
+            app_component_1.AppComponent,
+            hero_detail_component_1.HeroDetailComponent
+        ],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
